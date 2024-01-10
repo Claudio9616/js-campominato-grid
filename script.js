052c1fa9dda11c352811a9cc546a9553c5dd9f25
Claudio9616 <clares9611@gmail.com>
2024-01-10 18:30:25 +0100

    aggiunta prima parte della logica di js

--- script.js
+++ script.js
@@ -2,5 +2,18 @@ const button = document.getElementById('pulsante')
 const grid = document.getElementById('grid')
 const rows = 10
 const cols = 10
-const cells = rows * cols
-console.log(cells)
+const totCells = rows * cols
+const createCells = () => {
+    const cells = document.createElement('div')
+    cells.className = 'cells'
+    return cells
+}
+button.addEventListener('click', function(){
+    for (let i = 1; i <= totCells; i++){
+        const newCells = createCells()
+        newCells.addEventListener('click', () => {
+           newCells.classList.toggle('clicca')
+        })
+        grid.appendChild(newCells)
+    }
+})
